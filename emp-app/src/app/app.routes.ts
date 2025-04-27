4796766d4770c52f40e8e83e4264f3b5112518e6
kalharaweragoda <kalharaweragoda@gmail.com>
2025-04-27 09:28:20 +0530

    user sign in add

--- emp-app/src/app/app.routes.ts
+++ emp-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MamageEmpComponent } from './page/mamage-emp/mamage-emp.component';
 import { ViewAllEmployeeeComponent } from './page/view-all-employeee/view-all-employeee.component';
+import { UserSigninComponent } from './page/user-signin/user-signin.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path:"view-all-employee",
         component:ViewAllEmployeeeComponent
+    },
+    {
+        path:"user-signIn",
+        component:UserSigninComponent
     }
     
 ];
---
+++ emp-app/src/app/page/user-signin/user-signin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-signin',
+  imports: [],
+  templateUrl: './user-signin.component.html',
+  styleUrl: './user-signin.component.css'
+})
+export class UserSigninComponent {
+
+}
